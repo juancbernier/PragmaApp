8683cee0b9ff27c12b49ab72c03edc6d44a0489d
Juan Bernier <juanberniercr@unimagdalena.edu.co>
2023-06-02 16:05:22 -0500

    Initial Commit

---
+++ PragmaTest/PragmaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PragmaTestApp.swift
+//  PragmaTest
+//
+//  Created by Juan Bernier on 2/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PragmaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
